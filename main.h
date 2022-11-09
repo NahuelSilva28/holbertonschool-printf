4c48fd848b1cd7691a667a35e94bc54f4555d3fb
Franco-Olmos <5749@holbertonstudents.com>
2022-11-09 09:29:24 -0800

    main.h mod

--- main.h
+++ main.h
@@ -1,7 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*libraries*/
 #include <stdio.h>
 #include <unistd.h>
 #include <stdarg.h>
 #include <limits.h>
+
+/**
+* struct print - printf type with corresponding print function
+* @t: print type
+* @f: print function
+*/
+
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
+
