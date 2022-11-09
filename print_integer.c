79f82ed622dd4ba83fb130bc47437311a4f21489
Franco-Olmos <5749@holbertonstudents.com>
2022-11-09 10:23:04 -0800

    function print_int complete

--- print_integer.c
+++ print_integer.c
@@ -19,4 +19,21 @@ int print_int(va_list i)
 		op1 /= 10;
 		t[var1] = (f / op1) % 10;
 	}
+	if (f < 0)
+	{
+		_putchar('-');
+		count++;
+		for (var1 = 0; var1 < 10; var1++)
+			t[var1] *= -1;
+	}
+	for (var1 = 0, sum = 0; var1 < 10; var1++)
+	{
+		sum += t[var1];
+		if (sum != 0 || var1 == 9)
+		{
+			_putchar('0' + t[var1]);
+			count++;
+		}
+	}
+	return (count);
 }
