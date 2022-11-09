a63accdd9f8c0fdab5b6dfa7c56b2aa649d35955
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2022-11-09 10:53:46 -0800

    printffinish

--- _printf.c
+++ _printf.c
@@ -13,3 +13,33 @@ int _printf(const char *format, ...)
 
 	if (format == NULL)
 		return (-1);
+
+	va_start(lst, format);
+	while (format[i])
+	{
+		for (; format[i] != '%' && format[i]; i++)
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		if (!format[i])
+			return (count);
+		f = check(&format[i + 1]);
+		if (f != NULL)
+		{
+			count += f(lst);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
+	}
+	va_end(lst);
+	return (count);
+}
